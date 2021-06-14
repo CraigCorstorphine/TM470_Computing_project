b10a54d7c02afad733af2d22948aede66231592d
craigcorstorphine <emgpersonaltraining@gmail.com>
2021-06-14 15:35:44 +0100

    First commit

---
+++ app/src/main/java/com/craigcorstorphine/tm470_computing_project/DialogCloseListner.java
@@ -0,0 +1,7 @@
+package com.craigcorstorphine.tm470_computing_project;
+
+import android.content.DialogInterface;
+
+public interface DialogCloseListner {
+    public void handleDialogClose(DialogInterface dialog);
+}
---
+++ app/src/main/java/com/craigcorstorphine/tm470_computing_project/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.craigcorstorphine.tm470_computing_project;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+
+        final Intent i = new Intent(SplashActivity.this, MainActivity.class);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(i);
+                finish();
+            }
+        }, 2000);
+    }
+}
